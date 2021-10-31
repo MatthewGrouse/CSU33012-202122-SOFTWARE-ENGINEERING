e4e885a509fafd00e21b00ef693a8a34febe849b
Matthew Grouse <72737644+MatthewGrouse@users.noreply.github.com>
2021-10-31 19:15:32 +0000

    Committing DAG LCA to new branch

--- LCApython/LCA.py
+++ LCApython/LCA.py
@@ -22,10 +22,3 @@ def findLCA(root, n1, n2):
 
     return left_lca if left_lca is not None else right_lca
 
-root = Node(1)
-root.left = Node(2)
-root.right = Node(3)
-root.left.left = Node(4)
-root.left.right = Node(5)
-root.right.left = Node(6)
-root.right.right = Node(7)
