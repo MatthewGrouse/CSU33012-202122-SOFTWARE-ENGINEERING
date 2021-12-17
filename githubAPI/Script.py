e3cac9c6937f12ae6bb1365ebb82ce6dbd14456a
Matthew Grouse <72737644+MatthewGrouse@users.noreply.github.com>
2021-12-17 17:10:47 +0000

    Access to repo information

---
+++ githubAPI/Script.py
@@ -0,0 +1,3 @@
+# Install latest version of pip when you install python
+#Install PyGithub Library with
+# py -m pip install "SomeProject"
--- githubAPI/githubAPI.py
+++ githubAPI/githubAPI.py
@@ -1,1 +1,13 @@
-print("Initial Commit");
+from github import Github
+
+#insert github authorized token into line below
+g = Github("token")
+
+usr = g.get_user()
+print("user: " + usr.login)
+if usr.name is not None:
+    print("name: " + usr.name)
+if usr.location is not None:
+    print("location: " + usr.location)
+if usr.company is not None:
+    print("company: " + usr.company)
