7a24016d71c3a6db4b1d9c34673b9960c8143d71
Matthew Grouse <72737644+MatthewGrouse@users.noreply.github.com>
2021-12-17 17:19:58 +0000

    Introducted dictionary for subsequent data storage

--- githubAPI/githubAPI.py
+++ githubAPI/githubAPI.py
@@ -1,13 +1,16 @@
 from github import Github
+import json
 
 #insert github authorized token into line below
-g = Github("token")
+g = Github("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")
 
 usr = g.get_user()
-print("user: " + usr.login)
-if usr.name is not None:
-    print("name: " + usr.name)
-if usr.location is not None:
-    print("location: " + usr.location)
-if usr.company is not None:
-    print("company: " + usr.company)
+
+dct = {
+        'user': usr.login,
+        'name': usr.name,
+        'location':usr.location,
+        'company':  usr.company
+}
+
+print("dict is " + json.dumps(dct))
