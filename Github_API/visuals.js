ba5cd74b20f3b8935da17570443e1205a7197030
Matthew Grouse <72737644+MatthewGrouse@users.noreply.github.com>
2022-01-03 23:55:00 +0000

    updating api

--- Github_API/visuals.js
+++ Github_API/visuals.js
@@ -19,78 +19,26 @@ form.addEventListener('submit',function(e){
       </a>
       <div href = "https://github.com/${oName}">
       <br>
-        <h3>${oName} User Profile</h3>
-        Bio: <i>${data.bio==null?"He/She hasn't say anything":data.bio}</i>
+        <h3> ${oName}</h3>
+        Bio: <i>${data.bio==null?"No User Bio":data.bio}</i>
         <br>
-        Following: <i>${data.following==0?"He/She is not following anyone":data.following}</i>
+        Following: <i>${data.following==0?"This user doesn't follow anyone yet":data.following}</i>
         <br>
-        Followers: <i>${data.followers==0?"Nobody is following him/her, click on the profile image to follow👆":data.followers}</i>
+        Followers: <i>${data.followers==0?"This user has no followers":data.followers}</i>
         <br>
-        Date became a Github user: <i>${data.created_at}</i>
+        Date created: <i>${data.created_at}</i>
         <br>
         No. Repositories: <i>${data.public_repos}</i>
       </div>
       <!-- </body> -->
     `
   })
-
-
-
-    fetch("https://api.github.com/users/" + oName + "/followers")
-      .then((result1) => result1.json())
-      .then((dataF) =>{
-        console.log(dataF)
-        var first = dataF[0];
-        if(first!=null){
-            var firstName = first.login;
-            document.getElementById("resultF").innerHTML = `
-            <div>
-                <!-- <h6>First github user he/she follows:</h6> -->
-                First github user following him/her:
-                <a target ="_blank" href = "https://github.com/${firstName}">
-                 <img src = "${first.avatar_url}" width="100" height="100"/>
-                </a>
-                 (Click avatar to view)
-              </div>
-              <!-- </body> -->
-            `
-          }
-          else{
-
-          }
-
-        })
-
-
-
-
-            fetch("https://api.github.com/users/" + oName + "/following")
-              .then((result2) => result2.json())
-              .then((dataFF) =>{
-                console.log(dataFF)
-                var firstF = dataFF[0];
-                if(firstF!=null){
-                    var firstFName = firstF.login;
-                    document.getElementById("resultFF").innerHTML = `
-                    <div>
-                        First github user he/she follows:
-                        <a target ="_blank" href = "https://github.com/${firstFName}">
-                         <img src = "${firstF.avatar_url}" width="100" height="100"/>
-                        </a>
-                        (Click avatar to view)
-                      </div>
-                    `
-                  }
-                  else{
-
-                  }
-
-                })
 })
+  
 function handleInput(input) {
     var user = document.getElementById("search").value;
     user = user.replace(/\s/g, '');
-    var token = document.getElementById("authToken").value !== "" ? document.getElementById("authToken").value : undefined;
+    var token = undefined
         if (languageChart != null) languageChart;
         if (hourCommitChart != null) hourCommitChart;
         chartType = (input==2) ?    'doughnut':'bar';
@@ -256,7 +204,7 @@ function drawHour(cont, label, data) {
             labels: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10','11', '12', '13', '14', '15', '16', '17', '18', '19', '20','21', '22', '23', '24' ],
             datasets: [{
                 data: data,
-                label:"commit freq /hour",
+                label:"commits/hour",
                 borderWidth: 1,
                 hoverBorderWidth: 2,
 
@@ -291,6 +239,7 @@ function drawHour(cont, label, data) {
         },
     });
 }
+
 
 async function toggleChart() {
   // languageChart.destroy;
